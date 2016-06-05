cefb5805b63bff93794691bdfb5b5f7e633985ab
reztip <alexpitzer42@gmail.com>
2016-06-04 23:33:41 -0700

    thru titanic pandas tutorial, next is ML tutorial

--- pantut.py
+++ pantut.py
@@ -18,4 +18,27 @@ embarked_na_fill = 'X'
 df['emb'] = df['Embarked'].fillna(embarked_na_fill).map( \
         {'S': 0, 'C': 1,'Q': 2, embarked_na_fill: 3} ).astype(int)
 
+median_ages = np.zeros((2,3))
+for i in range(0,2):
+    for j in range(0,3):
+        median_ages[i,j] = df[(df['Gender'] == i) & \
+         (df['Pclass'] == j+1)]['Age'].dropna().median()
+
+# Feature generation
+df['AgeFill'] = df['Age']
+df['AgeIsNull'] = pd.isnull(df.Age).astype(int)
+df['FamilySize'] = df['SibSp'] + df['Parch']
+
+# Fill in missing age data with medians by class and gender
+for i in range(0,2):
+    for j in range(0,3):
+        df.loc[ (df.Age.isnull()) & (df.Gender == i) & (df.Pclass == j+1), \
+         'AgeFill'] = median_ages[i,j]
+
+df['AgexClass'] = df['AgeFill'] * df['Pclass']
+df = df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis = 1)
+df = df.drop(['Age'], axis = 1)
+df = df.dropna()
+
+train_data = df.values
 
